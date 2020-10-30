7782a765286ed2da3fa0e4f5a9d8af22f368e26e
Quincy Cheng <quincy.cheng@gmail.com>
2020-10-30 12:13:38 +0800

    Update env.sh

--- secretless/env.sh
+++ secretless/env.sh
@@ -2,12 +2,12 @@
 
 APP_NAME=testapp-secure
 APP_NAMESPACE=testapp
-APP_SERVICE_ACCOUNT_NAME=testap-secure-sa
+APP_SERVICE_ACCOUNT_NAME=testapp-secure-sa
 
 AUTHENTICATOR_ID="dev"
 
-APP_SECRETS_POLICY_BRANCH="apps/secrets/test"
-APP_SECRETS_READER_LAYER="apps/layers/myapp"
+APP_SECRETS_POLICY_BRANCH="app/testapp/secret"
+APP_SECRETS_READER_LAYER="app/testapp/layer"
 
 CONJUR_ACCOUNT="default"
 CONJUR_APPLIANCE_URL="https://conjur.demo.com"
