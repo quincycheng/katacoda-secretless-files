68e75fb0e00cb178ab6bde46062e99f0894da01f
Quincy Cheng <quincy.cheng@gmail.com>
2020-11-06 19:05:12 +0800

    Update env.sh

--- secretless/env.sh
+++ secretless/env.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 APP_NAME=testapp-secure
 APP_NAMESPACE=testapp
 APP_SERVICE_ACCOUNT_NAME=testapp-secure-sa
@@ -10,7 +8,6 @@ APP_SECRETS_POLICY_BRANCH="app/testapp/secret"
 APP_SECRETS_READER_LAYER="app/testapp/layer"
 
 CONJUR_ACCOUNT="default"
-#CONJUR_APPLIANCE_URL="https://conjur.demo.com"
 CONJUR_APPLIANCE_URL="https://conjur-cluster-conjur-oss.conjur-server.svc.cluster.local"
 
 CONJUR_ADMIN_AUTHN_LOGIN="admin"
@@ -18,6 +15,5 @@ CONJUR_ADMIN_API_KEY="MySecretP@ss1"
 
 OSS_CONJUR_SERVICE_ACCOUNT_NAME="conjur-cluster"
 OSS_CONJUR_NAMESPACE="conjur-server"
-
 
 APP_AUTHENTICATION_CONTAINER_NAME="secretless"
